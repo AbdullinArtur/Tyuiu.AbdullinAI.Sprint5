c8b87103a1c21142168eb0c1f8bda07c1fada2fe
Tyuiu.AbdullinAI@outlook.com <holchik919@gmail.com>
2023-11-19 22:50:10 +0500

    Task3

---
+++ Tyuiu.AbdullinAI.Sprint5.Task3.V17.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AbdullinAI.Sprint5.Task3.V17.Lib;
+using System.IO;
+
+namespace Tyuiu.AbdullinAI.Sprint5.Task3.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\hahaha\source\repos\Tyuiu.AbdullinAI.Sprint5\Tyuiu.AbdullinAI.Sprint5.Task3.V17\bin\Debug\OutPutFileTask3.bin";
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            bool wait = true;
+            bool fileExists = fileInfo.Exists;
+
+            Assert.AreEqual(wait, fileExists);
+            
+        }
+    }
+}
---
+++ Tyuiu.AbdullinAI.Sprint5.Task3.V17/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AbdullinAI.Sprint5.Task3.V17.Lib;
+
+
+namespace Tyuiu.AbdullinAI.Sprint5.Task3.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Абудллин А. И.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Абдуллин А. И. | АСОиУБ-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x = 3;
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!!");
+            Console.ReadKey();
+
+        }
+    }
+}
